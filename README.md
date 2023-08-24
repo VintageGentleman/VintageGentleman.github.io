c0b1daa65ff4c78838eb4861c302c1f3084c536d
VintageGT <114378858+VintageGentleman@users.noreply.github.com>
2023-08-24 14:32:14 +0900

    project1 update

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-- 👋 Hi, I’m @VintageGentleman
-- 👀 I’m interested in ... Java, JavaScript, HTML, CSS etc
-- 🌱 I’m currently learning ... Java
-- 💞️ I’m looking to collaborate on ... Some kind of project that I could learn coding. 
-- 📫 How to reach me ... Send me a message 
-
-<!---
-VintageGentleman/VintageGentleman is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+개인 포트폴리오 웹사이트 토이프로젝트입니다.
--- resources/js/common.js
+++ resources/js/common.js
@@ -8,3 +8,21 @@ $to_main_btn.addEventListener('click', function() {
     location.href='../index.html#project_section';
 });
 
+/**
+ * upbutton에 대한 동작 규정, 스크롤 Y 500 이상일 때 보여지게 하고
+ * 클릭할 경우 scrollTop을 0로 만듦.
+ */
+const $button_upbutton = document.querySelector('#upbutton');
+
+window.addEventListener('scroll', function() {
+    let scroll = document.documentElement.scrollTop;
+    if(scroll >= 500) {
+        $button_upbutton.style.display = "block";
+    } else {
+        $button_upbutton.style.display = "none";
+    }
+})
+
+$button_upbutton.addEventListener('click', function() {
+    window.scrollTo({top: 0, behavior:'instant'});
+});
